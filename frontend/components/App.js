827ce13ad44a887b4284cf239fec5654450c10cf
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-11-18 20:52:59 -0500

    Initial Commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,7 +7,43 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
+    const [planetInfo, setPlanetInfo] = useState([]);
+		const [peopleInfo, setPeopleInfo] = useState([]);
+
+		// ❗ Create effects to fetch the data and put it in state
+		useEffect(() => {
+			function getPlanets() {
+				axios
+					.get(urlPlanets)
+					.then((res) => {
+						setPeopleInfo(res.data);
+					})
+					.catch((err) => {
+						console.log(err.message);
+					});
+			}
+			function getPeople() {
+				axios
+					.get(urlPeople)
+					.then((res) => {
+						setPlanetInfo(res.data);
+					})
+					.catch((err) => {
+						console.log(err.message);
+					});
+			}
+			getPlanets();
+			getPeople();
+		}, []);
+
+		function testing() {
+			if (planetInfo.length > 0 && peopleInfo.length > 0) {
+				console.log(planetInfo);
+				console.log(peopleInfo);
+			}
+		}
+
+		testing();
   return (
     <div>
       <h2>Star Wars Characters</h2>
