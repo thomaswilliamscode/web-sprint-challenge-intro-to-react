b7044ab525416b75874d0edf9a02eae2abd7558a
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-11-20 15:28:09 -0500

    Sprint Complete

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Styles from 'styled-components'
+
+
 
 function Character(props) { // ❗ Add the props
-  console.log('In The Char')
-  console.log(props)
+  const {name, from} = props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  const [ showFrom, setShowFrom ] = useState(false)
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  function toggle() {
+    setShowFrom(!showFrom)
+  }
   return (
-		<div>
+		<div className='character-card' onClick={toggle}>
 			{/* Use the same markup with the same attributes as in the mock */}
-			{console.log('In The Char')}
+      <h3 className='character-name'>{name}</h3>
+      {
+        showFrom ? (
+        <p>
+          Planet:
+          <span className='character-planet'> {from}</span>
+        </p>) : null
+      }
+			
 		</div>
 	);
 }
