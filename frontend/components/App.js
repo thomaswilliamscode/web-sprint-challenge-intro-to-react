28d89a1076c38ed8d49308f3ab00d840d5f81e3a
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-11-20 13:41:30 -0500

    App is working, rendering Character

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -9,32 +9,34 @@ function App() {
   // ❗ Create state to hold the data from the API
     const [planetInfo, setPlanetInfo] = useState([]);
 		const [peopleInfo, setPeopleInfo] = useState([]);
+    const [newArray, setNewArray] = useState([])
+
+    const combinedInfo = []
 
 		// ❗ Create effects to fetch the data and put it in state
-		useEffect(() => {
-			function getPlanets() {
-				axios
-					.get(urlPlanets)
-					.then((res) => {
-						setPeopleInfo(res.data);
-					})
-					.catch((err) => {
-						console.log(err.message);
-					});
-			}
-			function getPeople() {
-				axios
-					.get(urlPeople)
-					.then((res) => {
-						setPlanetInfo(res.data);
-					})
-					.catch((err) => {
-						console.log(err.message);
-					});
-			}
-			getPlanets();
-			getPeople();
-		}, []);
+    useEffect( () => {
+      const promises = [
+        axios.get(urlPlanets),
+        axios.get(urlPeople)
+      ];
+
+      Promise.all(promises)
+      .then(([planetRes, peopleRes]) => {
+        setPlanetInfo(planetRes.data)
+        setPeopleInfo(peopleRes.data)
+
+        
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+    }, [])
+
+    //testing();
+		combineInfo();
+
+    
+    
 
 		function testing() {
 			if (planetInfo.length > 0 && peopleInfo.length > 0) {
@@ -43,14 +45,47 @@ function App() {
 			}
 		}
 
-		testing();
+    function combineInfo() {
+      peopleInfo.forEach( (person) => {
+        const {id, name, homeworld} = person
+        const answer = planetInfo.filter((info) => {
+          const {id} = info
+          return ( id === homeworld)
+        })
+        let from = answer[0].name
+        combinedInfo.push({id,name,from})
+      })
+      //console.log(combinedInfo)
+      if ((combinedInfo.length === 9) && (newArray.length === 0)) {
+        //console.log('new')
+				setNewArray(combinedInfo)
+        //console.log(newArray)
+			}
+      
+    }
+
+       
+        
+        
+    
+
   return (
-    <div>
-      <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
-    </div>
-  )
+		<div>
+			<h2>Star Wars Characters</h2>
+			<p>
+				See the README of the project for instructions on completing this
+				challenge
+			</p>
+			{/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {
+        newArray.map( info =>{
+          const {id, name, from} = info;
+          return <Character key={id} name={name} from={from} />
+        })
+      }
+
+		</div>
+	);
 }
 
 export default App
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-function Character() { // ❗ Add the props
+function Character(props) { // ❗ Add the props
+  console.log('In The Char')
+  console.log(props)
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
-    </div>
-  )
+		<div>
+			{/* Use the same markup with the same attributes as in the mock */}
+			{console.log('In The Char')}
+		</div>
+	);
 }
 
-export default Character
+export default Character;
